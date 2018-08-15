d765f513bc0f54b01ecb7d78e9140bf32a5c2aae
Alexey Alexeev <voodoo144@yandex.ru>
2018-08-15 13:48:43 +0300

    test

--- service_catalog_api.go
+++ service_catalog_api.go
@@ -1,11 +1,36 @@
 package main
+
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
-	"net/http"
 	"log"
+	"net/http"
+	"os"
 )
 
+type ServiceConfig struct {
+	Port          string
+	MysqlUser     string
+	MysqlPort     string
+	MysqlDatabase string
+}
+
+func GetServiceConfig() *ServiceConfig {
+	config := ServiceConfig{
+		Port:          os.Getenv("TEST_PORT"),
+		MysqlUser:     os.Getenv(""),
+		MysqlPort:     os.Getenv(""),
+		MysqlDatabase: os.Getenv(""),
+	}
+
+	return &config
+}
+
 func main() {
-	
+	config := GetServiceConfig()
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Println("test")
+	})
+	log.Fatal(http.ListenAndServe(":"+config.Port, router))
 }
