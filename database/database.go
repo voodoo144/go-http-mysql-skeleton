4224a2c99d34261df47997269650a6579a488b14
Alexey Alexeev <voodoo144@yandex.ru>
2018-08-16 11:53:46 +0300

    mysql and config refactoring

--- database/database.go
+++ database/database.go
@@ -1,36 +1,42 @@
 package database
 
 import (
+	. "ServiceCatalogApi/config"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
-	. "ServiceCatalogApi/config"
 )
 
-var db *gorm.DB
-var err error
-var dialect string = "mysql"
+var (
+	_db     *gorm.DB
+	err     error
+	dialect string = "mysql"
+)
 
 func OpenConnection() {
-	c:=GetServiceConfig()
-	db, err := gorm.Open(dialect, c.MysqlUser+":"+c.MysqlPassword+"@/"+c.MysqlDatabase+"?charset=utf8&parseTime=True&loc=Local")
+	c := GetServiceConfig()
+	_db, err := gorm.Open(dialect, c.MysqlUser+":"+c.MysqlPassword+"@/"+c.MysqlDatabase+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		log.Println(err)
 	}
-	defer db.Close()
+	defer _db.Close()
 }
 
 func ExecuteQuery(query string) {
-	if db == nil {
+	if _db == nil {
 		OpenConnection()
 	}
-	tx := db.Begin()
+	log.Println("Trying to execute query: " + query)
+	tx := _db.Begin()
 	if tx.Error != nil {
 		log.Println("Error starting transaction:")
 		log.Println(tx.Error)
 	}
 	if err := tx.Exec(query).Error; err != nil {
+		log.Println(err)
+		log.Println("Error in transaction, rolling back")
 		tx.Rollback()
 	}
+	log.Println("Commiting transaction")
 	tx.Commit()
 }
